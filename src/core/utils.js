68aaa4419ddedf46d76218d37c4d29ce99a728e5
cxxi <l.a_lumet@yahoo.fr>
2023-01-03 17:49:45 +0100

    DEV - try to fix os win32

--- src/core/utils.js
+++ src/core/utils.js
@@ -39,7 +39,12 @@ const env = {
 	dirname: path => url.fileURLToPath(new URL('.', path)),
 	cmdPath: name => env.win32support(`${env.dirname(import.meta.url)}../commands/${name.replace(':', '/')}.js`),
 	pkg: (key = false) => {
-		const pkgObject = JSON.parse(fs.readFileSync(env.win32support(`${env.dirname(import.meta.url)}../../package.json`)))
+		let path = env.win32support(`${env.dirname(import.meta.url)}../../package.json`)
+		console.log(`DEBUG: ${path}`)
+		let packageJson = fs.readFileSync(path)
+		console.log(`DEBUG: ${packageJson}`)
+		const pkgObject = JSON.parse(packageJson)
+		console.log('DEBUG:', pkgObject)
 		return key ? ( key in pkgObject ? pkgObject[key] : {}) : pkg
 	}
 }
